5d9d12ca1f90331eab26393901c578ff1f21d6e5
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-06-15 07:48:21 -0300

    Added enemy range Enemy provoked gun to the player

--- Assets/DataFiles/Scripts/EnemyAI.cs
+++ Assets/DataFiles/Scripts/EnemyAI.cs
@@ -7,7 +7,11 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
+
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,49 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            //chase
+            ChaseTarget();
+        }else if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            //attack
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
     }
+
+    void AttackTarget()
+    {
+        //Attack
+        print("Attack!");
+    }
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
 }
