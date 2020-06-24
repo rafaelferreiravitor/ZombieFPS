a590048d420ae1046595e7ef7b772cba197bfd8a
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-06-24 07:47:51 -0300

    Added a weapon zoom script

---
+++ Assets/DataFiles/Scripts/WeaponZoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    float zoomIn = 20f;
+    float zoomOut = 60f;
+    Camera fpsCamera;
+
+    private void Awake()
+    {
+        fpsCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            fpsCamera.fieldOfView = (fpsCamera.fieldOfView == zoomOut) ? zoomIn : zoomOut;
+        }
+    }
+}
