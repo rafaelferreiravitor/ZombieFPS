86c2927dd03ed0b27f0eee77a0bff27d00aa47b6
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-06-25 07:42:28 -0300

    Reduced mouse sensitivity on zoomed camera.

--- Assets/DataFiles/Scripts/WeaponZoom.cs
+++ Assets/DataFiles/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
@@ -9,16 +10,38 @@ public class WeaponZoom : MonoBehaviour
     float zoomOut = 60f;
     Camera fpsCamera;
 
+    float ZoomXSensitivity;
+    float ZoomYSensitivity;
+
+    float regularXSensitivity;
+    float regularYSensitivity;
+
+    RigidbodyFirstPersonController player;
+
     private void Awake()
     {
         fpsCamera = Camera.main;
+        var playerObject = GameObject.FindObjectOfType<RigidbodyFirstPersonController>();
+        player = playerObject.GetComponent<RigidbodyFirstPersonController>();
+
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            fpsCamera.fieldOfView = (fpsCamera.fieldOfView == zoomOut) ? zoomIn : zoomOut;
+            if (fpsCamera.fieldOfView == zoomOut)
+            {
+                fpsCamera.fieldOfView = zoomIn;
+                player.mouseLook.XSensitivity *= 0.5f;
+                player.mouseLook.YSensitivity *= 0.5f;
+            }
+            else
+            {
+                fpsCamera.fieldOfView = zoomOut;
+                player.mouseLook.XSensitivity *= 2f;
+                player.mouseLook.YSensitivity *= 2f;
+            }
         }
     }
 }
