fd2c35e756848f98ead2edfc246d346578dac03a
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-07-12 19:41:25 -0300

    Added a bunker to the scene and a flashlight to the player

---
+++ Assets/DataFiles/Scripts/FlashLight.cs
@@ -0,0 +1,39 @@
+﻿using Boo.Lang.Environments;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay*Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        myLight.spotAngle = (myLight.spotAngle <= minimumAngle) ? minimumAngle : myLight.spotAngle - angleDecay*Time.deltaTime;
+    }
+}
